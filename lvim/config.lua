b33e0cf03573a85f6867fbf08ee44857c7dbf7cd
socoolbear <socoolbear@gmail.com>
2023-08-19 22:10:22 +0900

    lvim update

--- lvim/config.lua
+++ lvim/config.lua
@@ -2,14 +2,7 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
-lvim.plugins = {
-    {
-        "voldikss/vim-floaterm",
-        config = function()
-          vim.g.floaterm_width = 0.8
-          vim.g.floaterm_height = 0.8
-          vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
-          vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>')
-        end
-    }
-}
+
+require('user/options')
+require('user/plugins')
+
---
+++ lvim/lua/user/plugins.lua
@@ -0,0 +1,11 @@
+lvim.plugins = {
+    {
+        "voldikss/vim-floaterm",
+        config = function()
+          vim.g.floaterm_width = 0.8
+          vim.g.floaterm_height = 0.8
+          vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
+          vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>')
+        end
+    }
+}

d0ba77979025c02c1d7af31b9e66d259edf82617
socoolbear <socoolbear@gmail.com>
2023-08-19 21:35:33 +0900

    - 개선

--- lvim/config.lua
+++ lvim/config.lua
@@ -3,5 +3,17 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 lvim.plugins = {
-    "voldikss/vim-floaterm"
+    {
+        "voldikss/vim-floaterm",
+        config = function()
+          vim.g.floaterm_width = 0.8
+          vim.g.floaterm_height = 0.8
+          vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
+          vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>')
+          vim.cmd([[
+            highlight link Floaterm CursorLine
+            highlight link FloatermBorder CursorLineBg
+          ]])
+        end
+    }
 }

b07d6dc04b38e27c6be160a7327915591f5a60a4
socoolbear <socoolbear@gmail.com>
2023-08-19 21:51:51 +0900

    - 개선

--- lvim/config.lua
+++ lvim/config.lua
@@ -10,10 +10,6 @@ lvim.plugins = {
           vim.g.floaterm_height = 0.8
           vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
           vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>')
-          vim.cmd([[
-            highlight link Floaterm CursorLine
-            highlight link FloatermBorder CursorLineBg
-          ]])
         end
     }
 }

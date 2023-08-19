658aac7ddf3a27161b5ed63a00801232c2e11a79
socoolbear <socoolbear@gmail.com>
2023-08-20 00:48:08 +0900

    - 개선

--- lvim/lua/user/plugins/lspconfig.lua
+++ lvim/lua/user/plugins/lspconfig.lua
@@ -59,7 +59,7 @@ vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
 vim.keymap.set('n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
 
 -- Commands
-vim.api.nvim_create_user_command('Format', vim.lsp.buf.formatting, {})
+-- vim.api.nvim_create_user_command('Format', vim.lsp.buf.formatting, {})
 
 -- Diagnostic configuration
 vim.diagnostic.config({

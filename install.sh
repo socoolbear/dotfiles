6b756f7286a1c94225521624cd298fac4b182c24
socoolbear <socoolbear@gmail.com>
2024-11-27 12:21:33 +0900

    - 작업 중

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ mkdir -p "$HOME"/.local/bin
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
     KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+    git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
 fi
 
 ln -sf "$DOTFILES"/zsh/zshrc "$HOME"/.zshrc
